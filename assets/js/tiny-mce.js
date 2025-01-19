f644a4d2d64e401a8d7d5c7c0df3c26f8429d1b3
danielwattimuryjr <daniel032902@gmail.com>
2025-01-19 15:30:03 +0800

    add static assets

---
+++ assets/js/tiny-mce.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const themeOptions = document.body.classList.contains("dark")
+    ? {
+        skin: "oxide-dark",
+        content_css: "dark",
+      }
+    : {
+        skin: "oxide",
+        content_css: "default",
+      };
+
+  tinymce.init({
+    selector: "#default",
+    plugins: "lists, link, image, media",
+    toolbar:
+      "h1 h2 bold italic strikethrough blockquote bullist numlist backcolor | link ",
+    menubar: false,
+    ...themeOptions,
+  });
+});
